ba6eda40c000da4eeb6f3df50c9d049a80f49063
felipe.sampedro <pipesancho182@gmail.com>
2022-10-10 16:48:15 -0500

    por fin termine

--- index.js
+++ index.js
@@ -1,42 +1,80 @@
-const express = require('express')
-const path = require('path')
-const {engine:handlebars}=require('express-handlebars')
+const express = require("express");
+const { Server: HttpServer } = require("http")
+const { Server: SocketServer } = require("socket.io") 
+// const path = require('path')
+// const {engine:handlebars}=require('express-handlebars')
 const Products = require('./model/products')
-
-const productsRoutes = require('./routes')
-
-const PORT = process.env.PORT || 8080
+const { formatMessage } = require("./utils/utils");
+// const productsRoutes = require('./routes')
 
 const app = express()
-const products = new Products()
+const PORT = process.env.PORT || 8080
+const httpServer = new HttpServer(app)
+const io = new SocketServer(httpServer)
+const productos = new Products()
 
-app.engine('hbs',handlebars({
-    extname:'hbs',
-    defaultLayout:'index.hbs',
-    layoutsDir: path.resolve(__dirname,'./views/layouts'),
-    partialsDir:path.resolve(__dirname,'./views/partials')
-}))
+// app.engine('hbs',handlebars({
+//     extname:'hbs',
+//     defaultLayout:'index.hbs',
+//     layoutsDir: path.resolve(__dirname,'./views'),
+//     partialsDir:path.resolve(__dirname,'./views/partials')
+// }))
 
-app.set('views','./views/layouts');
-app.set('view engine','hbs')
+// app.set('views','./views');
+// app.set('view engine','hbs')
 
-// app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.get('/',(req,res)=>{
-    res.render('main',{showp:true,products:products.getAll()})
-})
+// app.get('/',(req,res)=>{
+//     res.sendFile(__dirname + '/public/index.html')
+// })
 
-app.use('/productos',productsRoutes)
+// app.use('/productos',productsRoutes)
 
-app.use((err, req, res, next) => {
-	console.log(err);
-	res.status(500).json({ err, message: 'Something went wrong, sorry' });
-});
-
-app.listen(PORT, ()=>console.log("Ready an running on port", PORT))
-
-// connectedSever.on("error", (error)=> {
-//     console.log(`Something Went Wrong -> ERROR: `, error.message);
+// app.use((err, req, res, next) => {
+// 	console.log(err);
+// 	res.status(500).json({ err, message: 'Something went wrong, sorry' });
 // });
+
+httpServer.listen(PORT, ()=>console.log("Ready an running on port", PORT))
+
+const messages =[]
+const users =[]
+
+// const botName = "Shut bot"
+
+// Socket Events
+
+io.on('connection',(socket) =>{
+    console.log("A New Client Conection");
+
+    console.log(socket.id);
+    console.log(socket.username);
+
+    socket.emit('products', productos.getAll());
+
+    socket.on('newProduct', (newProduct) =>{
+        productos.save(newProduct);
+        io.sockets.emit('products', productos.getAll());        
+    });       
+
+    io.emit("message", [...messages]);
+
+    socket.on('newUser', (username) =>{
+        const newUser = {
+            id: socket.id,
+            username: username,
+        }
+        users.push(newUser);
+    });
+
+    socket.on("newMessage", (data) =>{
+        const user = users.find(user => user.id === socket.id);
+        const newMessage = formatMessage(socket.id, user.username, data);
+        messages.push(newMessage);
+        console.log(user.username);
+        io.emit('chatMessage', newMessage);
+    });
+});
